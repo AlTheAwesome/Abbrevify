9e88279d22262b888f47b1f7fcc07474fe2619a7
AlTheAwesome <altheawesome@yahoo.com>
2019-05-07 13:28:54 +0300

    different encoding method for symbols

--- Encode.py
+++ Encode.py
@@ -3,6 +3,7 @@ import string
 import random
 import database
 import datetime
+import sys
 alter = int(str(datetime.date.today())[-2:]) #He He
 
 
@@ -21,32 +22,42 @@ def name(word):
 
 variance = x = 0
 for i in range(len(raw)):
-	x = 1 if i%2 == 0 else -1 
+	x = 1 if i%2 == 0 else -1
 	if raw[i] in string.ascii_lowercase:
 		letter = raw[i]
 		set = database.SETS[random.randint(0, (len(database.SETS) - 2))] #set Select
 		abb = set[random.randint(0, (len(set) - 2))] #abbrev select
+		
 		while len(abb[raw[i]]) == 0:
 			abb = set[random.randint(0, (len(set) - 2))]
-	
-		pos = abb[raw[i]][random.randint(0, len(abb[raw[i]]) - 1)] #pos of letR	
+
+		pos = abb[raw[i]][random.randint(0, len(abb[raw[i]]) - 1)] #pos of letR
 		# print(name(abb['N'])[0].upper())
+
 		if pos > 9:
 			new = [str(pos)[0], set[-1] + name(abb['N'])[0].upper(), str(pos)[1]]
 		else:
 			new = ['0', set[-1] + name(abb['N'])[0].upper(), str(pos)[0]]
+
 		encoded.insert(x, new)
 		# print(encoded)
 	else:
 		SETX = database.SETX
+
 		if raw[i] in string.digits:
 			abb = SETX[random.randint(0, len(SETX) - 3)]
 			while abb[raw[i]] == []:
 				abb = SETX[random.randint(0, len(SETX) - 3)]
 		else:
 			abb = SETX[-2]
+
 		pos = abb[raw[i]][random.randint(0, len(abb[raw[i]]) - 1)]
-		new = ['0', SETX[-1] + name(abb['N'])[0].upper(), str(pos)[0]]
+		
+		if pos > 9:
+			new = [str(pos)[0], SETX[-1] + name(abb['N'])[0].upper(), str(pos)[1]]
+		else:
+			new = ['0', SETX[-1] + name(abb['N'])[0].upper(), str(pos)[0]]
+
 		encoded.insert(x, new)
 
 
@@ -59,4 +70,3 @@ print("".join(encoded))
 
 
 	# print(f"letter: {letter},\n abb: {name(abb['N']).upper()},\n position: {pos}")
-
